62e3dcf7f479218616b5a75799c0f4fea7412ddd
Liki Du <liki@likidu.com>
2023-01-08 01:06:23 -0800

    WIP 0108: OAuth

--- src/lib/services/client.ts
+++ src/lib/services/client.ts
@@ -2,5 +2,6 @@ import { login } from 'masto';
 
 export const client = await login({
   url: 'https://m.cmx.im',
-  accessToken: 'fM-4XXGcYHkJfAN7mM0Ap2OnX79UozEzjf7jd8ReXnY',
+  // accessToken: 'fM-4XXGcYHkJfAN7mM0Ap2OnX79UozEzjf7jd8ReXnY',
+  accessToken: 'iHudaJmvz-ZtaAv_pwQNFHn7YYiXDCBKdyeQsUmHB1Q',
 });

302afcaf7e58f5491d3a9e4ef378ef768dffa35a
Liya Du <liydu@microsoft.com>
2023-01-19 00:10:58 -0800

    Switch to use cmx.im for testing now

--- src/lib/configs/app.ts
+++ src/lib/configs/app.ts
@@ -2,11 +2,11 @@
  * Mastodon application settings
  */
 // cmx.im
-// export const CLIENT_ID = 'LkSupYPZ0Rm17yWEN4w_VzRIochPYfocFVRsD-o4Rhw';
-// export const CLIENT_SECRET = 'rI8I_5Ly8OoIJB4YTHngRCnE8hql0zY8Z60xXwWq5zk';
+export const CLIENT_ID = 'LkSupYPZ0Rm17yWEN4w_VzRIochPYfocFVRsD-o4Rhw';
+export const CLIENT_SECRET = 'rI8I_5Ly8OoIJB4YTHngRCnE8hql0zY8Z60xXwWq5zk';
 // hachyderm.io
-export const CLIENT_ID = 'XSR5kDhWDSXEaSEMsBCbi5nLasHrcCJP5rZXDJ6NV98';
-export const CLIENT_SECRET = '2Z17LQjT23Q_wBJ_gILaFCU3yoNXMzOuAb68OFFqqvo';
+// export const CLIENT_ID = 'XSR5kDhWDSXEaSEMsBCbi5nLasHrcCJP5rZXDJ6NV98';
+// export const CLIENT_SECRET = '2Z17LQjT23Q_wBJ_gILaFCU3yoNXMzOuAb68OFFqqvo';
 
 export const REDIRECT_URL_LOCAL = 'urn:ietf:wg:oauth:2.0:oob';
 export const REDIRECT_URL = 'https://likidu.github.io/webactivity-callback/kaistodon.html';
